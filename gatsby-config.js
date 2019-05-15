ee0a9b80e20e8481febe087b5631bfaa5a0405f4
Antonio Palmeira <av.palmeira@gmail.com>
2019-05-15 16:49:27 -0300

    fixed: ga was not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-{
+module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
           trackingId: "UA-140233648-1",
+          head: false
         }
     }
   ]
