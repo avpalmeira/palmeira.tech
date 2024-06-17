e251c480c8d5e2948dc58ee5b2538d10576bdd6c
Antonio V. Palmeira <av.palmeira@gmail.com>
2024-06-17 09:40:25 -0300

    Use new theme portfolio-minimal

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<img src="screenshot.png" alt="Gatsby Theme Portfolio Minimal Screenshot" width="700" />
-
 <a href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal-theme" target="_blank">
 <img src="https://www.gatsbyjs.com/deploynow.svg" alt="Deploy to Gatsby Cloud" />
 </a>
@@ -46,9 +44,3 @@ Just clone this Gatsby Starter, add the content. And that's it!
 If you find any bugs or have feature suggestions, create a new issue or pull request 🙏
 
 Thanks a lot for using this starter! 💪
-
----
-
-<a href="https://www.buymeacoffee.com/kmuenster" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
-
-Konstantin Münster – [konstantin.digital](https://konstantin.digital)
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,26 +3,29 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        // Used for sitemap generation
+        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/",
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
-          startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FFFFFF", // Used in manifest.json
-          themeColor: "#000000", // Used in manifest.json
-          display: "minimal-ui", // Used in manifest.json
+          // Path is relative to the root
+          favicon: "./content/images/favicon.ico",
+          // Used in manifest.json
+          siteName: "My Minimal Portfolio",
+          shortName: "Portfolio",
+          startUrl: "/",
+          backgroundColor: "#FFFFFF",
+          themeColor: "#000000",
+          display: "minimal-ui",
         },
         contentDirectory: "./content",
         blogSettings: {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+        googleAnalytics: {
+          trackingId: "UA-140233648-1",
+          anonymize: true, // Default true
+          environments: ["production"], // Default ["production"]
+        },
       },
     },
   ],
