720757263b1307f9512194926aeb1d26fcf33cee
Antonio V. Palmeira <av.palmeira@gmail.com>
2024-08-23 11:13:31 -0300

    add gtag script manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Script } from "gatsby";
 import {
   AboutSection,
   ContactSection,
@@ -10,9 +11,22 @@ import {
 } from "gatsby-theme-portfolio-minimal";
 
 export default function IndexPage() {
+  const hardCodedGoogleTrackingId = "G-07R4E822M3";
   return (
     <>
       <Seo title="The Portfolio Website of Antonio Palmeira" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${hardCodedGoogleTrackingId}`}
+        strategy="off-main-thread"
+      />
+      <Script id="gtag-config" strategy="off-main-thread" forward={[`gtag`]}>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag('js', new Date());
+          gtag('config', '${hardCodedGoogleTrackingId}')
+        `}
+      </Script>
       <Page>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About" />
